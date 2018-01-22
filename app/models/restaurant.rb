50d1dacb33955e81eea09fe467aff997a3d22164
kennedywong <kennedy.wong@live.hk>
2018-01-22 16:20:16 +0800

    add dependency between models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,9 @@
 class Restaurant < ApplicationRecord
-	validates_presence_of :name
-	mount_uploader :image, PhotoUploader
-	belongs_to :category
+  mount_uploader :image, PhotoUploader
+  validates_presence_of :name
 
-	has_many :comments
+  belongs_to :category
+
+  # 當 Restaurant 物件被刪除時，順便刪除依賴的 Comment
+  has_many :comments, dependent: :destroy
 end
