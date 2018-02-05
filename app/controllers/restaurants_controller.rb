ca28d02e5dd38606df1f173377e83fb3c18222ad
kennedywong <kennedy.wong@live.hk>
2018-02-05 10:10:51 +0800

    update favorites_count using counter_cache

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -25,7 +25,7 @@ class RestaurantsController < ApplicationController
   def favorite
     @restaurant = Restaurant.find(params[:id])
     @restaurant.favorites.create!(user: current_user)
-    @restaurant.count_favorites
+    #@restaurant.count_favorites
     redirect_back(fallback_location: root_path)
   end
 
@@ -34,7 +34,7 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     favorites = Favorite.where(restaurant: @restaurant, user: current_user)
     favorites.destroy_all
-    @restaurant.count_favorites
+    #@restaurant.count_favorites
     redirect_back(fallback_location: root_path)
   end
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -17,10 +17,10 @@ class Restaurant < ApplicationRecord
     self.favorited_users.include?(user)
   end
 
-  def count_favorites
-    self.favorite_count = self.favorites.size
-    self.save
-  end
+  #def count_favorites
+  #  self.favorites_count = self.favorites.size
+  #  self.save
+  #end
 
   def is_liked?(user)
     self.liked_users.include?(user)
