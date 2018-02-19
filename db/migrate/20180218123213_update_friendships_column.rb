c94a1b2bc58cc2623f55d0715df7df56f7d1512e
kennedywong <kennedy.wong@live.hk>
2018-02-19 21:48:43 +0800

    V2 - add friendship function

--- db/migrate/20180218123213_update_friendships_column.rb
+++ db/migrate/20180218123213_update_friendships_column.rb
@@ -1,5 +1,6 @@
 class UpdateFriendshipsColumn < ActiveRecord::Migration[5.1]
   def change
+  	rename_column :friendships, :following_id, :friend_id
   	add_column :friendships, :status, :boolean, default: false
   end
 end
