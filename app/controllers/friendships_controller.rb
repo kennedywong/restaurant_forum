9be662b8e51e3cf123e6b30d24557dbf143bee01
kennedywong <32028475+kennedywong@users.noreply.github.com>
2018-02-19 11:02:41 +0800

    Update friendships_controller.rb

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -16,7 +16,7 @@ class FriendshipsController < ApplicationController
   def destroy
     @friendship = current_user.friendships.where(friend_id: params[:id]).first
     @friendship.destroy
-    flash[:alert] = "Followship destroyed"
+    flash[:alert] = "刪除好友"
     redirect_back(fallback_location: root_path)
   end
 
